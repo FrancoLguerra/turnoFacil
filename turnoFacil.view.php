8f5e5c75998b178e2635999b427bd752295b7acd
Leandro Ramos <47448020+Leandro97ramos@users.noreply.github.com>
2022-10-24 12:37:09 -0300

    IMS-27  feat: :tada: add Listar-Medico

---
+++ turnoFacil.view.php
@@ -0,0 +1,27 @@
+<?php
+include_once('libs/smarty-master/libs/Smarty.class.php');
+
+class TurnoFacilView{
+    
+    private $smarty;
+    
+    public function __construct()
+    {
+        $this->smarty = new Smarty() ;
+    }
+    
+    function showHome(){
+    
+        $this->smarty->display('./templates/home.tpl');
+    }
+
+    
+    function showMedicsAssigned($medics){
+
+        $this->smarty->assign('medics', $medics);
+        $this->smarty->display('./templates/list_medic.tpl');
+
+    }
+
+    
+}

a0039659a9dfbe90fc806a399d9eab7becd78953
Leandro Ramos <47448020+Leandro97ramos@users.noreply.github.com>
2022-10-24 19:22:51 -0300

    IMS-27 feat: 🎉 add Listar-Medico

---
+++ models/turnoFacil.models.php
@@ -0,0 +1,36 @@
+<?php
+//los modelos lo usamos para consultas al servidor
+
+
+class TurnoFacilModel{
+    //private $db;
+    /* private $dbUrl = $cleardb_db;
+    private $dbSv = $cleardb_server;
+    private $dbUsr= $cleardb_username;
+    private $dbPss = $cleardb_password;
+    */
+    
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new PDO('mysql:host=localhost;'.'dbname=tpe_metodologia_2022;charset=utf8','root','');
+    }
+
+
+
+
+
+    function getMedicsAssigned($id){
+
+        $query = $this->db->prepare('SELECT *  from  medico JOIN secretaria on medico.id_secretaria  = secretaria.id_secretaria  WHERE secretaria.id_secretaria =?');
+
+        $query->execute([$id]);
+
+        return $query->fetchAll(PDO::FETCH_OBJ);
+
+
+    }
+
+
+}

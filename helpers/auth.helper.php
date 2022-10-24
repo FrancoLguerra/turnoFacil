b946c2871ca072e7e7386b3e72188310a1211f33
Leandro Ramos <47448020+Leandro97ramos@users.noreply.github.com>
2022-10-24 19:18:04 -0300

    IMS-29 🎉 feat: add ingreso al sistema

---
+++ helpers/auth.helper.php
@@ -0,0 +1,37 @@
+<?php
+
+class AuthHelper {
+    function __construct() {
+        // abre la sessión siempre para usar el helper
+        if (session_status() != PHP_SESSION_ACTIVE) {
+            session_start();
+        }
+    }
+ /*   public function logAdmin($user){
+        $_SESSION['ADMIN_ID'] = $user->id_user;
+        
+        
+        $_SESSION['USER_EMAIL'] = $user->email;
+    }*/
+    
+    public function login($user) {
+        $_SESSION['USER_ID'] = $user->id_secretaria;
+        $_SESSION['USER_NOMBRE'] = $user->nombre;
+        
+        
+    }
+
+
+    public function checkLoggedIn() {
+        if (empty($_SESSION['USER_ID'])) {
+            header("Location: " . LOGIN);
+            die();
+        }
+    }
+
+    function logout() {
+        session_destroy();
+        header("Location: " . BASE_URL );
+        die();
+    } 
+}
---
+++ router.php
@@ -0,0 +1,82 @@
+<?php
+
+
+require_once './controllers/turnoFacil.controller.php';
+
+require_once './controllers/auth.controller.php';
+
+define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
+
+define('LOGIN', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/login');
+
+if (!empty($_GET['action'])) {
+    $action = $_GET['action'];
+} else {
+    $action = 'home';
+}
+
+
+$params = explode('/', $action);
+switch ($params[0]) {
+    case 'home':
+        $controller = new TurnoFacilController();
+        $controller->showHome();
+        break;
+    case 'medicos-a-cargo':
+        $controller = new TurnoFacilController();
+        $controller->showMedicsAssigned();
+        break;
+
+
+
+
+
+
+
+        /*Listo*/ 
+
+
+        //form login
+    case 'login':
+        $authController = new AuthController();
+        $authController->showLogin();
+        break;
+
+        //verifico login
+    case 'verify': 
+        $authController = new AuthController();
+        $authController->login();
+        break;
+
+        
+
+
+
+
+        //ciero session
+    case 'logout':
+        $authController = new AuthController();
+        $authController->logout();
+        break;
+
+        /* CIERRRE DE Listo*/ 
+
+
+
+        //muestra pantalla registro
+    case 'register':
+        $authController = new AuthController();
+        $authController->showRegister();
+    break;
+    
+        //crea registro
+    case 'regUser':
+        $authController = new AuthController();
+        $authController->register();
+    break;
+
+
+        /*
+
+    */
+}
